5cbe9b17dc28cc3d5ad24a213f4ec362fe254176
Monishson <69966105+Monishson@users.noreply.github.com>
2020-08-24 16:07:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var ball,ballImg,paddle,paddleImg;
+var edges;
+function preload() {
+  /* preload your images here of the ball and the paddle */
+  ballImg=loadImage("ball.png");
+  paddleImg=loadImage("paddle.png");
+}
+function setup() {
+  createCanvas(400, 400);
+   /* create the Ball Sprite and the Paddle Sprite */
+  /* assign the images to the sprites */
+  ball=createSprite(200,200,20,20);
+  ball.addImage(ballImg);
+  paddle=createSprite(380,200,20,50);
+  paddle.addImage(paddleImg);
+  /* give the ball an initial velocity of 9 in the X direction */
+  ball.velocityX=9;
+  
+}
+
+function draw() {
+  background(205,153,0);
+  /* create Edge Sprites here */
+  edges=createEdgeSprites();
+  /* Allow the ball sprite to bounceOff the left, top and bottom edges only, leaving the right edge of the canvas to be open. */
+  ball.bounceOff(edges[0]);
+  ball.bounceOff(edges[2]);
+  ball.bounceOff(edges[3]);
+
+  /* Allow the ball to bounceoff from the paddle */
+  /* Also assign a collision callback function, so that the ball can have a random y velocity, making the game interesting */
+  ball.bounceOff(paddle,randomVelocity);
+  
+  /* Prevent the paddle from going out of the edges */ 
+  
+  
+  if(keyDown(UP_ARROW))
+  {
+     /* what should happen when you press the UP Arrow Key */
+   paddle.y=paddle.y-2;
+  }
+  
+  if(keyDown(DOWN_ARROW))
+  {
+    /* what should happen when you press the UP Arrow Key */
+    paddle.y=paddle.y+2;
+  }
+  
+  drawSprites();
+  
+}
+
+function randomVelocity()
+{
+  /* this function gets called when the ball bounces off the paddle */
+   ball.velocityY=random(-5,-8);
+  /* assign the ball a random vertical velocity, so it bounces off in random direction */
+}
+
